042874c2f685b639807e8909469d7aa3f8e2de5d
Faiz <faizhadiyanfirza@gmail.com>
2023-08-07 14:53:22 +0700

    git add executed

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Home from './screens/home';
+import AddNote from './screens/addNote';
+import EditNote from './screens/editNote';
 
-export default function App() {
+const addNote = (title, desc) => {
+  const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+
+  setNoteList([
+    ...noteList,
+    {
+      id,
+      title: title,
+      desc: desc,
+    },
+  ]);
+};
+
+const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote }) => {
+  switch (currentPage) {
+    case 'home':
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />;
+    case 'add':
+      // Berikan function "addNote" ke component "AddNote"
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case 'edit':
+      return <EditNote />;
+    default:
+      return <Home />;
+  }
+};
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: 'Note pertama',
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+    },
+  ]);
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CurrentPageWidget
+      currentPage={currentPage}
+      noteList={noteList}
+      setCurrentPage={setCurrentPage}
+      // Berikan function addNote sebagai prop
+      addNote={addNote}
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
